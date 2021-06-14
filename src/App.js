3056b2b63b64e66dab1a21c4524c454d01d00b26
sakinfens <sakinfens@gmail.com>
2021-06-14 00:36:11 -0500

    delete unused file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import Header from "./Components/Header";
 import EventsBanner from "./Components/EventsBanner"
-import Slider from "./Components/Slider";
 import ImageCarousel from "./Components/Carousel"
 import Events from "./Components/Events";
 import Contact from "./Components/Contact"
